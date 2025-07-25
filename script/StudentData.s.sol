88df74feddb82f3e3f461a9fafbc7332b96a9fd7
Meshack Yaro <yaromeshack@gmail.com>
2025-07-25 01:22:32 +0100

    deploy student data with foundry

---
+++ script/StudentData.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+import {Script, console} from "forge-std/Script.sol";
+import {StudentData} from "../src/StudentData.sol";
+
+contract StudentDataScript is Script {
+    StudentData public studentData;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        studentData = new StudentData("Mesh",25);
+
+        vm.stopBroadcast();
+    }
+}
